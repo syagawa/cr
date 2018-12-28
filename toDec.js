facb35156c8011b5495341c2a27d6e3d6889a746
syagawa <yagawa@qzc.co.jp>
2018-12-28 23:13:05 +0900

    Change read write method

--- toDec.js
+++ toDec.js
@@ -1,13 +1,15 @@
 const decrypt = require("./decrypt.js");
 const fs = require("fs");
-let b = process.argv[2];
-let pp = process.argv[3];
 
-const mode = b;
-if(mode === "file"){
-  let file = process.argv[3];
-  b = fs.readFileSync(file, "utf8");
+const mode = process.argv[2];
+let b, pp;
+
+if(mode === "-t"){
+  b = process.argv[3]
   pp = process.argv[4];
+}else{
+  b = fs.readFileSync(mode, "utf8");
+  pp = process.argv[3];
 }
 
 const d = decrypt(b, pp);
--- toEnc.js
+++ toEnc.js
@@ -1,14 +1,16 @@
 const encrypt = require("./encrypt.js");
 const fs = require("fs");
 
-let tx = process.argv[2];
-let pp = process.argv[3];
+const mode = process.argv[2];
 
-const mode = tx;
-if(mode === "file"){
-  let file = process.argv[3];
-  tx = fs.readFileSync(file, "utf8");
+let tx, pp;
+
+if(mode === "-t"){
+  tx = process.argv[3];
   pp = process.argv[4];
+}else{
+  tx = fs.readFileSync(mode, "utf8");
+  pp = process.argv[3];
 }
 
 const b = encrypt(tx, pp);
